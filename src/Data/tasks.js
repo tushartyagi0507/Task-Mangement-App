37456c2addef15887d671427751376910fcca31b
Tushar Tyagi <tushartyagi0507@gmail.com>
2024-10-19 02:26:18 +0530

    feat: add functionality

--- src/Data/tasks.js
+++ src/Data/tasks.js
@@ -1,4 +1,4 @@
-export const initialTasks = [
+export const Data = [
     { id: 1, title: 'Complete project', description: 'Finish the React project', priority: 'high', completed: false },
     { id: 2, title: 'Buy groceries', description: 'Get milk and bread', priority: 'medium', completed: false },
     { id: 3, title: 'Exercise', description: '30 minutes of jogging', priority: 'low', completed: true },
--- src/component/TaskMangement.js
+++ src/component/TaskMangement.js
@@ -1,36 +1,44 @@
 "use client";
 import React, { useState } from "react";
 
-export const TaskMangement = () => {
+export const TaskMangement = ({handleAdd}) => {
+
   const [task, settask] = useState("");
   const [description, setdescription] = useState("");
   const [priority, setpriority] = useState('')
 
+
   const handleSubmit = (e)=> {
-    console.log("working")
     e.preventDefault()
+    // console.log("Task:", task, description, priority)
+  handleAdd(task, description, priority)
+  setdescription("")
+  settask("")
+  setpriority("Select Priority")
   }
 
   const handlePriority = (e)=> {
-    console.log("priority is:", e.target.value)
+    setpriority(e.target.value)
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form id="form" onSubmit={handleSubmit}>
         <input
           type="text"
+          id="task-input"
           value={task}
           placeholder="Add a task"
           onChange={(e) => settask(e.target.value)}
         />
         <input
           type="text"
+          id="description-input"
           value={description}
           placeholder="Describe the task"
           onChange={e => setdescription(e.target.value)}
         />
         <select onChange={handlePriority}>
-            <option >Select Priority</option>
+            <option value="Select Priority">Select Priority</option>
           <option value="High" >
             High
           </option>
